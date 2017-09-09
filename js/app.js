9c0bc637a394f5cfa00d90a47e36a2514f1ad706
Tobie Langel <tobie@codespeaks.com>
2017-09-09 23:07:16 +0200

    Add basic app

---
+++ js/app.js
@@ -0,0 +1,61 @@
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+
+var currentWord = null;
+var words = null;
+var counter = 0;
+
+function next() {
+    document.querySelector("#input").value = "";
+    document.querySelector("#input").select()
+    if (!words || !words.length) {
+        words = shuffle(Object.keys(data));
+    }
+    currentWord = words.pop();
+    document.querySelector("img").src = data[currentWord];
+    document.querySelector("#msg").style.display = "none";
+    counter = 0
+}
+
+
+document.querySelector("#form").onsubmit = function(e) {
+    e.preventDefault();
+    if (currentWord == document.querySelector("#input").value.trim()) {
+        next();
+    } else if (counter > 2) {
+        document.querySelector("#msg").innerHTML = "Oops! Il s'agit du mot <em>" + currentWord + "</em>.";
+    } else {
+        counter++;
+        document.querySelector("#msg").style.display = "block";
+        document.querySelector("#msg").innerHTML = "Ce n'est pas tout à fait ça. Essaye encore!";
+    }
+    document.querySelector("#input").select()
+}
+
+function preload() {
+    Object.values(data).forEach(function(src) {
+        var img = new Image();
+        img.src = src;
+    });
+}
+preload();
+
+next();
---
+++ js/data.js
@@ -0,0 +1,18 @@
+var data = {
+    "l'école": "https://c1.staticflickr.com/3/2491/3849991325_95d6c053bd.jpg",
+    "la règle": "https://c1.staticflickr.com/1/14/15372633_4cf8d987e5.jpg",
+    "le cahier": "https://c2.staticflickr.com/4/3448/3317949531_630533e088.jpg",
+    "le classeur": "https://c1.staticflickr.com/5/4091/5037640657_a4bfd52bc3.jpg",
+    "le crayon": "https://c1.staticflickr.com/9/8113/15524060340_e487a4a8dc.jpg",
+    "la gomme": "https://c1.staticflickr.com/1/20/73226917_404e2f4a02.jpg",
+    "le maître": "https://c1.staticflickr.com/2/1164/1477899923_5fb736f33c.jpg",
+    "la maîtresse": "https://c1.staticflickr.com/5/4325/35394007073_4ac05e0e0f.jpg",
+    "l'élève": "https://c1.staticflickr.com/4/3753/10718160665_98086251b3.jpg",
+    "le tableau": "https://c1.staticflickr.com/1/31/57302289_95e43f06cd.jpg",
+    "le banc": "https://c1.staticflickr.com/1/636/32728322035_0416e32341.jpg",
+    "la chaise": "https://c1.staticflickr.com/8/7196/6885506646_ce36bc4185.jpg",
+    "nouveau": "https://c1.staticflickr.com/4/3932/15281134147_f397c20ea8.jpg",
+    "nouvelle": "https://c1.staticflickr.com/4/3860/14670135259_14b21beb13.jpg",
+    "découvrir": "https://c1.staticflickr.com/4/3040/2597970259_692ba1ed9b.jpg",
+    "calculer": "https://c1.staticflickr.com/7/6170/6175310592_e3c962f1dc.jpg"
+}
