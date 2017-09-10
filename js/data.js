87d6cd346e342a2c4f4609464845bfcd53059822
Tobie Langel <tobie@codespeaks.com>
2017-09-10 07:50:43 +0200

    Add english data

--- js/data.js
+++ js/data.js
@@ -16,3 +16,22 @@ var data = {
     "découvrir": "https://c1.staticflickr.com/4/3040/2597970259_692ba1ed9b.jpg",
     "calculer": "https://c1.staticflickr.com/7/6170/6175310592_e3c962f1dc.jpg"
 }
+
+var data_english = {
+    "school": "https://c1.staticflickr.com/3/2491/3849991325_95d6c053bd.jpg",
+    "ruler": "https://c1.staticflickr.com/1/14/15372633_4cf8d987e5.jpg",
+    "notebook": "https://c2.staticflickr.com/4/3448/3317949531_630533e088.jpg",
+    "binder": "https://c1.staticflickr.com/5/4091/5037640657_a4bfd52bc3.jpg",
+    "pencil": "https://c1.staticflickr.com/9/8113/15524060340_e487a4a8dc.jpg",
+    "eraser": "https://c1.staticflickr.com/1/20/73226917_404e2f4a02.jpg",
+    "teacher": "https://c1.staticflickr.com/2/1164/1477899923_5fb736f33c.jpg",
+    "teacher": "https://c1.staticflickr.com/5/4325/35394007073_4ac05e0e0f.jpg",
+    "student": "https://c1.staticflickr.com/4/3753/10718160665_98086251b3.jpg",
+    "blackboard": "https://c1.staticflickr.com/1/31/57302289_95e43f06cd.jpg",
+    "bench": "https://c1.staticflickr.com/1/636/32728322035_0416e32341.jpg",
+    "chair": "https://c1.staticflickr.com/8/7196/6885506646_ce36bc4185.jpg",
+    "new": "https://c1.staticflickr.com/4/3932/15281134147_f397c20ea8.jpg",
+    "new": "https://c1.staticflickr.com/4/3860/14670135259_14b21beb13.jpg",
+    "discover": "https://c1.staticflickr.com/4/3040/2597970259_692ba1ed9b.jpg",
+    "calculate": "https://c1.staticflickr.com/7/6170/6175310592_e3c962f1dc.jpg"
+}
